63261bb8edbfba01941b945b73a389d787b51daf
Charles Araújo <charles_araujo@ymail.com>
2021-10-12 23:38:32 -0300

    new: get current location and add api key

--- src/core.tsx
+++ src/core.tsx
@@ -1,15 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { requestForegroundPermissionsAsync } from "expo-location";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import apiKey from "../weatherapi";
 
 const Core: React.FC = () => {
-  const load = async () => {
-    let teste = await requestForegroundPermissionsAsync();
-    console.log(teste);
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const load = async (): Promise<void> => {
+    try {
+      let { status } = await requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMessage("Precisamos acessar a Localização para buscar o clima");
+        return;
+      }
+      const { latitude, longitude } = (await getCurrentPositionAsync()).coords;
+      console.log(latitude, longitude);
+    } catch (error) {}
   };
 
-  React.useEffect(() => {
+  React.useEffect((): void => {
     load();
   }, []);
 
---
+++ weatherapi.example.tsx
@@ -0,0 +1,11 @@
+/**
+ * Please visit https://openweathermap.org/
+ * and get your own api key
+ *
+ * Rename this file to:
+ * weatherapi.tsx
+ */
+
+const WEATHER_API_KEY = "abcde";
+
+export default WEATHER_API_KEY;
