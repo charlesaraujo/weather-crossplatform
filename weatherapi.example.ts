226b9dad70316e9d63b9bc2914584b763b53888e
Charles Araújo <charles_araujo@ymail.com>
2021-10-16 13:41:03 -0300

    refact: change tsx to ts

---
+++ weatherapi.example.ts
@@ -0,0 +1,13 @@
+/**
+ * Please visit https://openweathermap.org/
+ * and get your own api key
+ *
+ * Rename this file to:
+ * weatherapi.ts
+ */
+
+const WEATHER_API_KEY = "abcde";
+const API_URL = "https://api.openweathermap.org/data/2.5/";
+const IMG_URL = "https://openweathermap.org/img/wn/";
+
+export { WEATHER_API_KEY, API_URL, IMG_URL };
--- weatherapi.ts
+++ weatherapi.ts
@@ -3,10 +3,10 @@
  * and get your own api key
  *
  * Rename this file to:
- * weatherapi.tsx
+ * weatherapi.ts
  */
 
-const WEATHER_API_KEY = "abcde";
+const WEATHER_API_KEY = "56fa6bd214fa244ec84c1375ee5c19b7";
 const API_URL = "https://api.openweathermap.org/data/2.5/";
 const IMG_URL = "https://openweathermap.org/img/wn/";
 
