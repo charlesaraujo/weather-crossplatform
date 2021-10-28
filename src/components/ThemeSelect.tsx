6d2d34868b64ae2252ac42e43cdb3c05499cd67b
Charles Araújo <charles_araujo@ymail.com>
2021-10-28 13:02:09 -0300

    new: start themming

---
+++ src/components/ThemeSelect.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Colors } from "../utils/Colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: "stretch",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: 1,
+  },
+  wrapper: {
+    width: 80,
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  picker: {
+    width: 30,
+    height: 30,
+    marginBottom: 10,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.PRIMARY_COLOR,
+    borderWidth: 0,
+  },
+  text: {
+    // fontSize: 18,
+    // textAlign: "center",
+    justifyContent: "center",
+    // color: "#fff",
+  },
+  textSelected: {
+    color: Colors.SECONDARY_COLOR,
+  },
+  selected: {
+    borderWidth: 1,
+    borderColor: Colors.BORDER_COLOR,
+    backgroundColor: "#fff",
+  },
+});
+interface UnitsPickerProps {
+  unitsSystem: "metric" | "imperial";
+  setUnitsSystem: Function;
+}
+
+export const UnitsPicker: React.FC<UnitsPickerProps> = ({
+  unitsSystem,
+  setUnitsSystem,
+}) => {
+  const isMetric = unitsSystem === "metric" ? true : false;
+  return (
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        <TouchableOpacity
+          style={[styles.picker, isMetric && styles.selected]}
+          onPress={() => {
+            setUnitsSystem("metric");
+          }}
+        >
+          <View style={[styles.text]}>
+            <MaterialCommunityIcons
+              name="temperature-celsius"
+              size={24}
+              color={isMetric ? Colors.SECONDARY_COLOR : "#fff"}
+            />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.picker, !isMetric && styles.selected]}
+          onPress={() => {
+            setUnitsSystem("imperial");
+          }}
+        >
+          <View style={[styles.text]}>
+            <MaterialCommunityIcons
+              name="temperature-fahrenheit"
+              size={24}
+              color={!isMetric ? Colors.SECONDARY_COLOR : "#fff"}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
--- src/utils/Colors.ts
+++ src/utils/Colors.ts
@@ -1,15 +1,29 @@
-export const Colors = {
-  PRIMARY_COLOR: "#ff304f",
-  SECONDARY_COLOR: "#002651",
-  BORDER_COLOR: "#dbdbdb",
-};
+interface ColorScheme {
+  PRIMARY_COLOR: "#ff304f" | "#002651";
+  SECONDARY_COLOR: "#002651" | "#ff304f";
+  BORDER_COLOR: "#dbdbdb";
+  BACKGROUD_COLOR: "#ffffff" | "#000000";
+}
 
 /**
  * Finish with dark options
  * save a defatul theme in storage and change when user use darkmode
  */
-export const ColorsDark = {
+const ColorsDark: ColorScheme = {
   PRIMARY_COLOR: "#ff304f",
   SECONDARY_COLOR: "#002651",
   BORDER_COLOR: "#dbdbdb",
+  BACKGROUD_COLOR: "#ffffff",
 };
+const ColorsLight: ColorScheme = {
+  PRIMARY_COLOR: "#ff304f",
+  SECONDARY_COLOR: "#002651",
+  BORDER_COLOR: "#dbdbdb",
+  BACKGROUD_COLOR: "#000000",
+};
+
+const selectTheme = (): ColorScheme => {
+  return ColorsLight;
+};
+
+export const Colors: ColorScheme = selectTheme();
